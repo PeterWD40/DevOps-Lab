659904207b58ae22613c50faee3768e6dfe01906
peterwd40 <peterwd40@gmail.com>
2020-04-16 14:29:48 -0400

    new test

--- selenium/pythong_org_search.py
+++ selenium/pythong_org_search.py
@@ -1,3 +1,4 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,12 +7,12 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1420,1080')
-chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
-#driver = webdriver.Chrome()
+
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
@@ -20,3 +21,7 @@ elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
