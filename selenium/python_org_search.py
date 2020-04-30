7a8df0a2b30bab74f9cb22a96a854512bc3c3147
peterwd40 <peterwd40@gmail.com>
2020-04-30 13:59:09 -0400

    selenium: admin login test

---
+++ selenium/python_org_search.py
@@ -0,0 +1,27 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("--remote-debugging-port=9222")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ selenium/webapp_user_login.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--window-size=1420,1080')
+#	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument("--remote-debugging-port=9222")
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://192.168.0.42/webapp/login.php")
+        self.assertIn("Log", driver.title)
+        elem = driver.find_element_by_name("username")
+        elem.send_keys("super")
+        elem = driver.find_element_by_name("password")
+        elem.send_keys("super123")
+        elem.send_keys(Keys.RETURN)
+        self.assertIn("Welcome", driver.page_source)
+        #assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
